88c8361b20d39c0f6a4899a09d450f8134b2e538
DmitryBunakov <floppy-a@yandex.ru>
2023-06-06 21:49:09 +0500

    restore

--- src/javacalculatorurfu/Calculator.java
+++ src/javacalculatorurfu/Calculator.java
@@ -1,18 +1,9 @@
 package javacalculatorurfu;
-import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
 
-        double num1;
-        double num2;
-        double ans;
-        char op;
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Enter two numbers: ");
-        num1 = reader.nextDouble();
-        num2 = reader.nextDouble();
-        System.out.print("\nEnter an operator (+, -, *, /): ");
-        op = reader.next().charAt(0);
+        System.out.println("Hello calculator");
 
     }
 }
