f828b0ab8eb0f7cccebb0e612723076c6c58e017
DmitryBunakov <floppy-a@yandex.ru>
2023-06-06 20:00:14 +0500

    feat implement input data

--- src/javacalculatorurfu/Calculator.java
+++ src/javacalculatorurfu/Calculator.java
@@ -1,9 +1,18 @@
 package javacalculatorurfu;
-
+import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
 
-        System.out.println("Hello calculator");
+        double num1;
+        double num2;
+        double ans;
+        char op;
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
+        num1 = reader.nextDouble();
+        num2 = reader.nextDouble();
+        System.out.print("\nEnter an operator (+, -, *, /): ");
+        op = reader.next().charAt(0);
 
     }
 }
