cad0eaeb8d40088eddf044d7bb2d25d9133a1ea0
AlexanderJ <felmireni@gmail.com>
2023-06-06 17:39:55 +0500

    Del operators functions

--- src/javacalculatorurfu/Calculator.java
+++ src/javacalculatorurfu/Calculator.java
@@ -16,43 +16,11 @@ public class Calculator {
         a = sc.nextDouble();
         b = sc.nextDouble();
 
-        System.out.println("a = " +a+ " b = " +b);
+        System.out.println("a = " + a + " b = " + b);
 
         System.out.println("Enter the operator (+,-,*,/):");
 
         Character operator = sc.next().charAt(0);
         Double result = null;
-
-        switch (operator) {
-            case '+':
-                result = a + b;
-                break;
-
-            case '-':
-                result = a - b;
-                break;
-
-            case '*':
-                result = a * b;
-                break;
-
-            case '/':
-                if (b == null && b != 0.0) {
-                    result = a / b;
-                } else {
-                    System.out.println("Divide to zero! Change b number!");
-                }
-                break;
-
-            default:
-                System.out.println("Wrong input");
-        }
-
-        if(result == null){
-            System.out.println("Something went wrong");
-        } else {
-            System.out.println("The final result:");
-            System.out.println(a + " " + operator + " " + b + " = " + result);
-        }
     }
 }
