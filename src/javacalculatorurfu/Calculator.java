1c1dee6961f524d502a8a3c4c3dfb1957376ab3d
AlexanderJ <felmireni@gmail.com>
2023-06-06 15:33:35 +0500

    Add init main variables a, b

--- src/javacalculatorurfu/Calculator.java
+++ src/javacalculatorurfu/Calculator.java
@@ -1,9 +1,20 @@
 package javacalculatorurfu;
 
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
 
+        double a = 0.0;
+        double b = 0.0;
+
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Hello calculator");
+        System.out.println("Enter the numbers:");
+
+        a = sc.nextDouble();
+        b = sc.nextDouble();
 
     }
 }
