82d3789cb5339e2d76d5d873faa75681d3f43b27
kelalex <kel_alex174@mail.ru>
2023-06-07 14:43:15 +0500

    plus/minus

--- src/javacalculatorurfu/Calculator.java
+++ src/javacalculatorurfu/Calculator.java
@@ -4,23 +4,13 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-
-        Double a = 0.0;
-        Double b = 0.0;
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Hello calculator");
-        System.out.println("Enter the numbers:");
-
-        a = sc.nextDouble();
-        b = sc.nextDouble();
-
-        System.out.println("a = " + a + " b = " + b);
-
-        System.out.println("Enter the operator (+,-,*,/):");
-
-        Character operator = sc.next().charAt(0);
-        Double result = null;
+        switch (operator) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+        }
     }
 }
